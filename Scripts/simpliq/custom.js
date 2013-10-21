64d634bada3ba14f2a9a22e933e9e8ee28d9fae9
hong yu <hello.hyu@gmail.com>
2013-10-21 17:56:48 +0800

    datatable

--- Scripts/simpliq/custom.js
+++ Scripts/simpliq/custom.js
@@ -263,8 +263,10 @@ function template_functions(){
 
 	/* ---------- Datable ---------- */
 	$('.datatable').dataTable({
-			"sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span12'i><'span12 center'p>>",
-			"sPaginationType": "bootstrap",
+			"bPaginate": false,
+            "bLengthChange":false,
+            "bFilter":false,
+            "bInfo":false,
 			"oLanguage": {
 			"sLengthMenu": "_MENU_ records per page"
 			}
